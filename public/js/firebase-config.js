d6f8591411e9ded6e880f89bed0e30e51819798d
Teilon Santos <teilon66@gmail.com>
2026-05-31 22:50:00 -0300

    chore: separação das credenciais firebase em arquivo não-versionado

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,16 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
-
-const firebaseConfig = {
-  apiKey: "API_KEY_REMOVED",
-  authDomain: "conselho-tutelar-2f88e.firebaseapp.com",
-  projectId: "conselho-tutelar-2f88e",
-  storageBucket: "conselho-tutelar-2f88e.firebasestorage.app",
-  messagingSenderId: "1063421591986",
-  appId: "1:1063421591986:web:ce74854f2a5219a3d9e4ab",
-  measurementId: "G-RWYYYNT8MS"
-};
+import { firebaseConfig } from "./firebase-keys.js";
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
---
+++ public/js/firebase-keys.example.js
@@ -0,0 +1,10 @@
+// Renomeie este arquivo para firebase-keys.js e preencha com suas credenciais
+// Obtenha as credenciais em: https://console.firebase.google.com/
+export const firebaseConfig = {
+  apiKey: "SUA_API_KEY_AQUI",
+  authDomain: "seu-projeto.firebaseapp.com",
+  projectId: "seu-projeto",
+  storageBucket: "seu-projeto.firebasestorage.app",
+  messagingSenderId: "000000000000",
+  appId: "1:000000000000:web:xxxxxxxxxxxx"
+};
